292ae0b1039f71ad580d61bc25182b0afc2cf54b
Nilesh Rathi <nileshrathi01@gmail.com>
2017-05-30 00:44:32 +0530

    using db to persist notified state

--- roku.js
+++ roku.js
@@ -1,9 +1,18 @@
 const rp = require('request-promise');
-const slack = require('./slack')
+const slack = require('./slack');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB_URI);
 
-const API_URI = "http://52.66.182.145:5000/v1/currentStatus";
+let model = require('./model');
+
+const API_URI = process.env.API_URI || "http://52.66.182.145:5000/v1/currentStatus";
 const NO_API_REPONSE = "Uh-oh, Looks like the Raspberry Pi is down.";
+const DB_FAIL_CREATE = "Uh-oh, Database Create failed";
+const DB_FAIL_UPDATE = "Uh-oh, Database Update failed";
+
 const SLACK_CHANNEL = process.env.SLACK_CHANNEL || "@nr";
+const DEVICE_NAME = process.env.DEVICE_NAME || "southbeach";
 
 const ALLOWED_HOURS = [3, 5, 7, 9, 11]
 
@@ -23,6 +32,54 @@ function getAirQualityMessage(index) {
 		return "quality is at end of the world levels";	
 }
 
+function handleAPIFailure() {
+	model.find({ name: DEVICE_NAME }, function (err, res) {
+		if (err) {
+			slack.sendMessage("@nr", DB_FAIL_GET);
+			mongoose.disconnect();
+		}
+		else {
+			data = res[0];
+			if (data.notified) {
+				console.log("yay! we are done!");
+				mongoose.disconnect();
+			}				
+			else {
+				slack.sendMessage(SLACK_CHANNEL, NO_API_REPONSE);
+				updateDB(data.timestamp, true);
+			}
+				
+		}
+	});
+}
+
+function updateDB(ts, notified) {
+	model.findOneAndUpdate({ name: DEVICE_NAME }, { name: DEVICE_NAME, timestamp: ts, notified: notified }, function (err, res) {
+		if (err) {
+			slack.sendMessage("@nr", DB_FAIL_UPDATE);
+			mongoose.disconnect();
+		}
+		if (res == null) {
+			newDevice = model({
+				name: DEVICE_NAME,
+				timestamp: ts,
+				notified: notified
+			});
+			newDevice.save(function (err) {
+				if (err) {
+					slack.sendMessage("@nr", DB_FAIL_CREATE);
+					mongoose.disconnect();
+				}
+				else {
+					mongoose.disconnect();
+				}
+			});
+		}
+		else
+			mongoose.disconnect();
+	});
+}
+
 rp(options)
 	.then(function (data) {
 		if (!!data) {
@@ -36,10 +93,11 @@ rp(options)
 				message += `and the air ${airQuality} (${data.readingAQ}ppm).`
 				slack.sendMessage(SLACK_CHANNEL, message);
 			}
+			updateDB(data.timeStamp, false);
 		}
 		else {
-			slack.sendMessage(SLACK_CHANNEL, NO_API_REPONSE);
+			handleAPIFailure();
 		}
 	}, function () {
-		slack.sendMessage(SLACK_CHANNEL, NO_API_REPONSE);
+		handleAPIFailure();
 	});
