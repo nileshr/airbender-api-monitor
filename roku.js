db4e0031077a8c76906f443f09195ad48b25a943
Nilesh Rathi <nileshrathi01@gmail.com>
2017-05-29 10:55:21 +0530

    post updates multiple times a day

--- roku.js
+++ roku.js
@@ -5,6 +5,8 @@ const API_URI = "http://52.66.182.145:5000/v1/currentStatus";
 const NO_API_REPONSE = "Uh-oh, Looks like the Raspberry Pi is down.";
 const SLACK_CHANNEL = process.env.SLACK_CHANNEL || "@nr";
 
+const ALLOWED_HOURS = [3, 5, 7, 9, 11]
+
 const options = {
 	uri: API_URI,
 	json: true
@@ -16,7 +18,7 @@ rp(options)
 			let date = new Date();
 			let hour = date.getHours();
 			let minute = date.getMinutes();
-			if (hour == 3 && minute >= 30 && minute <= 39) { // Send message only once a day
+			if (ALLOWED_HOURS.includes(hour) && minute >= 30 && minute <= 39) { // Send message only once a day
 				let message = `The current temperature at \`${data.name}\` is \`${data.celsius}°C\` `
 				message += `, the humidity is \`${data.humidity}%\` `
 				message += `and the air quality index is \`${data.readingAQ}ppm\`.`
